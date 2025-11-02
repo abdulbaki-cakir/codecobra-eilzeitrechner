681b73d163ed80b20fa65854e8de56e1ec6d9e0b
Abdulbaki Cakir <abcakir@stud.hs-heilbronn.de>
2025-11-02 11:04:46 +0100

    add linters to pipeline

---
+++ teilzeit-ausbildungsrechner/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+
+    // 'standard' prüft die Code-Logik.
+    // 'prettier' (als letztes) schaltet alle Stil-Regeln von 'standard' ab,
+    // die mit Prettier in Konflikt stehen.
+    extends: [
+        'standard',
+        'prettier'
+    ],
+
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+
+    // Das 'rules'-Objekt ist jetzt leer.
+    // ESLint (via 'standard') kümmert sich um die Logik-Fehler.
+    // Prettier kümmert sich um die Formatierung.
+    rules: {
+        // Hier kommen nur noch spezielle Regeln rein, wenn du
+        // von 'standard' oder 'prettier' abweichen WILLST.
+        // Wir lassen es für den Moment leer.
+    }
+}
